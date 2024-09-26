4eb5194d01aab14c559c89e09f457a7c7d560c5b
Maksym Solokha <maksim.solokha0411@gmail.com>
2024-09-26 14:18:42 +0300

    fix(vite): fix vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/test-task-pics-io/',
     test: {
         globals: true,
         environment: 'jsdom',
