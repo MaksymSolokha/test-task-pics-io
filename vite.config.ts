8105af6baee82567e0c81e65b593d55bea109e84
Maksym Solokha <maksim.solokha0411@gmail.com>
2024-09-26 13:55:58 +0300

    feature(deploy): add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/test-task-pics-io/',
     test: {
         globals: true,
         environment: 'jsdom',
